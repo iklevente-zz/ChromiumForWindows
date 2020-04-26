ad3cec7bdb5126cc106c44adfb6b47ecc6b15831
iklevente <k.levi2@outlook.hu>
2020-04-26 12:50:08 +0200

    MAJOR UPDATE ALMOST READY
    Yep :) Backend works almost perfectly. Only need to get the frontend UI communicating with backend code and I need to make the new installer.
    Also I need to update the version checker.
    
    - Material frontend GUI
    - Comatible with Hibbiki's Chromium
    - Shortcut support done
    - Kill Chrome process to be able to update
    - Set as Default Browser support is done (Thanks to Hibbiki's installer)

---
+++ FixShortcut.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using IWshRuntimeLibrary;
+
+namespace ChromiumForWindows
+{
+    class FixShortcut
+    {
+        public static void MakeShortcuts()
+        {
+            DesktopShortcut();
+            StartMenuShortcut();
+        }
+
+        // Delete Chromium desktop shortcut and replace with the updater's shortcut:
+        private static void DesktopShortcut()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            System.IO.File.Delete(Path.Combine(desktopPath, "Chromium.lnk"));
+
+            // Make new shortcut: 
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\Chromium.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "Open Chromium";
+            shortcut.TargetPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Chromium\\ChromiumLauncher.exe";
+            shortcut.Save();
+        }
+
+        // Delete Chromium Start Menu shortcut and replace with the updater's shortcut:
+        private static void StartMenuShortcut()
+        {
+            string startMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Windows\\Start Menu\\Programs\\";
+            System.IO.File.Delete(Path.Combine(startMenuPath, "Chromium.lnk"));
+
+            // Make new shortcut:
+            string pathToExe = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Chromium\\ChromiumLauncher.exe";
+            string StartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+            string appStartMenuPath = Path.Combine(StartMenuPath, "Programs");
+
+            if (!Directory.Exists(appStartMenuPath))
+            {
+                Directory.CreateDirectory(appStartMenuPath);
+            }
+
+            string shortcutLocation = Path.Combine(appStartMenuPath, "Chromium" + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = "Open Chromium";
+            //shortcut.IconLocation = @"C:\Program Files (x86)\TestApp\TestApp.ico"; //uncomment to set the icon of the shortcut
+            shortcut.TargetPath = pathToExe;
+            shortcut.Save();
+        }
+    }
+}
---
+++ Update.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChromiumForWindows
+{
+    class Update
+    {
+        public static void StartUpdate()
+        {
+            // Closes all Chromium tasks to be able to update:
+            Console.WriteLine("Closing Chromium, if opened...");
+            Process[] processes = Process.GetProcessesByName("Chrome");
+            foreach (var process in processes)
+            {
+                process.Kill();
+                System.Threading.Thread.Sleep(4000);
+                Console.WriteLine("Chromium process killed!");
+            }
+
+            // Deletes old installer if exists:
+            if (File.Exists(MainWindow.chromiumPath + "\\mini_installer.sync.exe"))
+            {
+                System.IO.File.Delete(MainWindow.chromiumPath + "\\mini_installer.sync.exe");
+                Console.WriteLine("Deleted old Chromium installer (mini_installer.sync.exe).");
+            }
+
+            // Changing version info to the latest one:
+            System.IO.File.WriteAllText(MainWindow.chromiumPath + "\\versioninfo.txt", MainWindow.latestVersion);
+            Console.WriteLine("There is a new Chromium out there! Updating...");
+            Console.WriteLine("Updating versioninfo.txt is done! The program will update Chromium to the latest version now!");
+
+
+            if (Directory.Exists(MainWindow.chromiumPath + "\\Application"))
+            {
+                Console.WriteLine("Deleting old Chromium...");
+                DeleteOldChromium();
+                Directory.Delete(MainWindow.chromiumPath + "\\Application");
+                Console.WriteLine("Done!");
+            }
+
+            // Downloading and updating Chromium to the latest version:
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile("https://github.com/Hibbiki/chromium-win64/releases/latest/download/mini_installer.sync.exe", MainWindow.chromiumPath + "\\mini_installer.sync.exe");
+            }
+            Console.WriteLine("Latest Chromium installer (mini_installer.sync.exe) downloaded");
+
+            Console.WriteLine("Installing...");
+            System.Diagnostics.Process.Start(System.IO.Path.Combine(MainWindow.chromiumPath + "\\mini_installer.sync.exe"));
+            System.Threading.Thread.Sleep(7000);
+        }
+
+        static void DeleteOldChromium()
+        {
+            DirectoryInfo chromiumDir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Chromium\\Application\\");
+
+            foreach (FileInfo file in chromiumDir.GetFiles())
+            {
+                System.Threading.Thread.Sleep(5000); // NEEDS FIX
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in chromiumDir.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+    }
+}
