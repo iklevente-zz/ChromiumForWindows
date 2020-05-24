d0c041bc60ee418ac8e51d85f89f9e921335732e
iklevente <k.levi2@outlook.hu>
2020-05-24 16:20:13 +0200

    Added Installer project and will be working on a settings manager

---
+++ ChromiumForWindowsInstaller/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChromiumForWindowsInstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AcceptPipe_Click(object sender, RoutedEventArgs e)
+        {
+            if (CountinueButton.IsEnabled == false)
+            {
+                CountinueButton.IsEnabled = true;
+            }
+            else if (CountinueButton.IsEnabled == true)
+            {
+                CountinueButton.IsEnabled = false;
+            }
+        }
+    }
+}
